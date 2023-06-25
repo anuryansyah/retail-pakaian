79562bb4f9185938dd7cb2eb75f7ff4deabb9bdb
Alifudin Nuryansyah <yansyah.nur1@gmail.com>
2023-06-25 13:34:41 +0700

    add-search-product

--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const { authToken } = require('../controllers/AuthController')
-const { addProduct, getAllProduct } = require('../controllers/ProductController')
+const { addProduct, getAllProduct, searchProduct } = require('../controllers/ProductController')
 
 const router = express.Router()
 
 router.get('/produk', getAllProduct)
 router.post('/produk', addProduct)
+router.get('/produk/search', searchProduct)
 
 module.exports = router
